d062f41ddcbe42e85e2246dc07ab9dcea6a84196
eytanav <eytan.attard-vella.24@um.edu.mt>
2025-09-28 11:37:28 +0200

    Initial commit

---
+++ cpp-programming/devcmd.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Set up MSVC environment
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+:: Change to folder where batch file is located
+cd /d "%~dp0"
+
+cmd
