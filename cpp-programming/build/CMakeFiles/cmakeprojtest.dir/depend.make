7919acf9bb632e3fad7341498f28ac705d55d9fe
eytanav <eytan.attard-vella.24@um.edu.mt>
2025-10-08 21:37:45 +0200

    Setup

---
+++ cpp-programming/build/CMakeFiles/cmakeprojtest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmakeprojtest.
+# This may be replaced when dependencies are built.
---
+++ cpp-programming/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eytan/uni-coding/CPS2004-OOP/CPS2004-OOP/cpp-programming
+# Build directory: /home/eytan/uni-coding/CPS2004-OOP/CPS2004-OOP/cpp-programming/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
