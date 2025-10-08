21762fc36a3f95558683a54f827e1dd3f135817f
eytanav <eytan.attard-vella.24@um.edu.mt>
2025-10-08 21:05:05 +0200

    Resetting repo

--- cpp-programming/devcmd.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Set up MSVC environment
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-
-:: Change to folder where batch file is located
-cd /d "%~dp0"
-
-cmd
--- cpp-programming/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, this is a simple testing program!" << std::endl;
-    return 0;
-}
